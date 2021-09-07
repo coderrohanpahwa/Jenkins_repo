cb5b172f3b649f1cc21a9cf8e77f5e80ae79a849
coderrohanpahwa <63921650+coderrohanpahwa@users.noreply.github.com>
2021-09-07 16:23:32 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+class URLTest(TestCase):
+  def example_test(self):
+    print("Invoekd in tests")
+    response=self.client.get('/')
+    
 # Create your tests here.
