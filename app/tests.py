07d291bf014511448c036fc654793aa4b5b87434
coderrohanpahwa <63921650+coderrohanpahwa@users.noreply.github.com>
2021-09-07 16:36:02 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -3,5 +3,5 @@ class URLTest(TestCase):
   def example_test(self):
     print("Invoekd in tests")
     response=self.client.get('/')
-    
+    self.assertEquals(response.status_code,200)
 # Create your tests here.
